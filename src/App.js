a3c6a2223c72b76be47c1150bb638d6fdbe32bee
Ahmed Dhaouadi <Dapbarka@gmail.com>
2020-11-08 20:04:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Pilot from '../src/profile/Pilot.png'
 
 
 function App() {
-  const Alert = () => alert('Ahmed Dhaouadi')
+  const handlName = () => alert('Ahmed Dhaouadi')
   return (
     <div>
 
 
-      <Profile FullName='Ahmed Dhaouadi'  profession='Helicopter Pilot' bio='Welcome to my Blog' alert={Alert}>
+      <Profile FullName='Ahmed Dhaouadi'  profession='Helicopter Pilot' bio='Welcome to my Blog' alert={handlName}>
 
         <img style={{ borderRadius: '5px', opacity: '80%' }} src={Pilot} alt="not really me" />
        
